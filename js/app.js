3dc9ab6abb7a3bc6629c773a774ef88bdca5b827
dbryandallas <dustin.bryan@gmail.com>
2016-05-15 13:13:41 -0400

    add variables

--- js/app.js
+++ js/app.js
@@ -20,8 +20,34 @@ $("a.new").click(function(){
   newGame();
 });
 
+$("#guessButton").click(function(event) {
+        event.preventDefault();
+        guess();
+    });
+});
+
 /*generate random number between 1-100*/
 
 function getSecretNumber() {
         return Math.floor((Math.random() * 100) + 1);
+}
+
+/*variables*/
+
+var userGuess;
+var feedBack;
+var secretNumber;
+var guessCount;
+var guessList;
+
+function newGame() {
+  disableInput(false);
+  secretNumber =getSecretNumber();
+
+  
+  userGuess = "";
+  feedBack = "Make your Guess!";
+  guessCount = 0;
+  guessList = [];
+  updateDisplay ();
 }
